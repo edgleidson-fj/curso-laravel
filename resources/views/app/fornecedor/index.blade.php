ad91dc82e275b4cf042171ec2ff8fa2fe2be353f
Edgleidson <edgleidson.jesus>
2022-12-27 08:31:00 -0300

    Blade - @empty

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,7 +2,16 @@
 
 @php
     /*
-    if(isset(variavel)) {} //isset retorna True se a variavel estiver definida
+    if(empty(variavel)) {} //empty retorna True se a variavel estiver vazia
+    Ex: 
+    - '' 
+    - 0 
+    - 0.0 
+    - '0' 
+    - null 
+    - false 
+    - array() 
+    - variavel que foi criada sem valor
     */
 @endphp
 
@@ -15,11 +24,17 @@
     Status: {{$fornecedores[0]['status']}}<br>
     @isset($fornecedores[0]['cnpj'])
         CNPJ: {{$fornecedores[0]['cnpj']}}<br>
+        @empty($fornecedores[0]['cnpj']) <!--empty retorna True se a variavel estiver vazia-->
+            - CNPJ vazio 1
+        @endempty
     @endisset
     <hr>
     Fornecedor: {{$fornecedores[1]['nome']}}<br>
     Status: {{$fornecedores[1]['status']}}<br>
     @isset($fornecedores[1]['cnpj'])
         CNPJ: {{$fornecedores[1]['cnpj']}}<br>
+        @empty($fornecedores[1]['cnpj'])
+            - CNPJ vazio 2
+        @endempty
     @endisset
 @endisset
