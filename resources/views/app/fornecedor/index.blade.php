dd2b7c994e03bcad604a2e7272de2fa2e72b3f4f
Edgleidson <edgleidson.jesus>
2022-12-27 08:08:43 -0300

    Blade - @isset

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,19 +2,24 @@
 
 @php
     /*
-    if(condicao) {} //if executa enquanto a condição for True
+    if(isset(variavel)) {} //isset retorna True se a variavel estiver definida
     */
 @endphp
 
 {{--@dd($fornecedores);--}}
 
-{{--@unless executa enquanto o resultado for falso--}}
 
-Fornecedor: {{$fornecedores[0]['nome']}}<br>
-Status: {{$fornecedores[0]['status']}}<br><br>
-
-@unless($fornecedores[0]['status'] == 'S') <!--unless executa enquanto o resultado for falso -->
-    Fornecedor inativo
-@endunless
-<br>
-
+<!--isset retorna True se a variavel estiver definida-->
+@isset($fornecedores)
+    Fornecedor: {{$fornecedores[0]['nome']}}<br>
+    Status: {{$fornecedores[0]['status']}}<br>
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{$fornecedores[0]['cnpj']}}<br>
+    @endisset
+    <hr>
+    Fornecedor: {{$fornecedores[1]['nome']}}<br>
+    Status: {{$fornecedores[1]['status']}}<br>
+    @isset($fornecedores[1]['cnpj'])
+        CNPJ: {{$fornecedores[1]['cnpj']}}<br>
+    @endisset
+@endisset
