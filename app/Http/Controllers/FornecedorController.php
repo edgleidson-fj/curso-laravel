5736969a056bff1836f8aa0f2292ef77c9ccb847
Edgleidson <edgleidson.jesus>
2022-12-29 07:58:27 -0300

    Blade - @switch/case

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -6,15 +6,28 @@ use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $fornecedores = [
-            0=>[
-                'nome'=>'Fornecedor 1', 
-                'status'=>'N', 
-                'cnpj'=>''],
-            1=>[
-                'nome'=>'Fornecedor 2', 
-                'status'=>'S']    
+            0 => [
+                'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '212121212121212',
+                'ddd' => '11', //São Paulo (SP)
+                'telefone' => '0000-0000'
+            ],
+            1 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S',
+                'ddd' => '71', //Salvador (BA)
+                'telefone' => '0000-0000'
+            ],
+            2 => [
+                'nome' => 'Fornecedor 3',
+                'status' => 'S',
+                'ddd' => '85', //Fortaleza (CE)
+                'telefone' => '0000-0000'
+            ]
         ];
 
         return view('app.fornecedor.index', compact('fornecedores'));
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -22,8 +22,21 @@
 @isset($fornecedores)
     Fornecedor: {{$fornecedores[1]['nome']}}<br>
     Status: {{$fornecedores[1]['status']}}<br>
-    CNPJ: {{$fornecedores[1]['cnpj'] ?? 'Dado não foi preenchido'}}<br> <!-- ?? -> Valor default-->
-    <!-- 
-       variavel testada não estiver definida(isset) ou possui valor null 
-    -->
+    CNPJ: {{$fornecedores[1]['cnpj'] ?? 'Dado não foi preenchido'}}<br> <!-- ?? -> Valor default-->   
+    Telefone: ({{$fornecedores[1]['ddd'] ?? ''}}) {{$fornecedores[1]['telefone'] ?? ''}}<br> <!--(00) 0000-0000 -->
+
+    <!--Switch Case-->
+    @switch($fornecedores[1]['ddd'])
+        @case('11')
+            São Paulo - SP 
+            @break
+        @case('71')
+            Salvador - BA 
+            @break 
+        @case('85')
+            Fortaleza - CE 
+            @break 
+        @default
+            Estado não identificado    
+    @endswitch
 @endisset
