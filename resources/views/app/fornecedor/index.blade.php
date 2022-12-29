d5281095257e5a3d4175739c8dac985030c6c25d
Edgleidson <edgleidson.jesus>
2022-12-29 08:17:54 -0300

    Blade - @for

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -20,23 +20,12 @@
 
 <!--isset retorna True se a variavel estiver definida-->
 @isset($fornecedores)
-    Fornecedor: {{$fornecedores[1]['nome']}}<br>
-    Status: {{$fornecedores[1]['status']}}<br>
-    CNPJ: {{$fornecedores[1]['cnpj'] ?? 'Dado não foi preenchido'}}<br> <!-- ?? -> Valor default-->   
-    Telefone: ({{$fornecedores[1]['ddd'] ?? ''}}) {{$fornecedores[1]['telefone'] ?? ''}}<br> <!--(00) 0000-0000 -->
-
-    <!--Switch Case-->
-    @switch($fornecedores[1]['ddd'])
-        @case('11')
-            São Paulo - SP 
-            @break
-        @case('71')
-            Salvador - BA 
-            @break 
-        @case('85')
-            Fortaleza - CE 
-            @break 
-        @default
-            Estado não identificado    
-    @endswitch
+    <!--for -->
+    @for($i=0; isset($fornecedores[$i]); $i++)
+        Fornecedor: {{$fornecedores[$i]['nome']}}<br>
+        Status: {{$fornecedores[$i]['status']}}<br>
+        CNPJ: {{$fornecedores[$i]['cnpj'] ?? 'Dado não foi preenchido'}}<br> <!-- ?? -> Valor default-->   
+        Telefone: ({{$fornecedores[$i]['ddd'] ?? ''}}) {{$fornecedores[$i]['telefone'] ?? ''}}<br> <!--(00) 0000-0000 -->
+        <hr>
+    @endfor      
 @endisset
