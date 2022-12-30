d94d9a03c38f874cc63d05f185d7d1e486c165a6
Edgleidson <edgleidson.jesus>
2022-12-30 08:33:45 -0300

    Blade - @forelse

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -20,12 +20,14 @@
 
 <!--isset retorna True se a variavel estiver definida-->
 @isset($fornecedores)
-    <!--foreach -->
-    @foreach($fornecedores as $indice => $fornecedor)
+    <!--forelse - Obs: Se por acaso o Array estiver vazio, o fluxo será desviado para o bloco empty -->
+    @forelse($fornecedores as $indice => $fornecedor)
         Fornecedor: {{$fornecedor['nome']}}<br>
         Status: {{$fornecedor['status']}}<br>
         CNPJ: {{$fornecedor['cnpj'] ?? 'Dado não foi preenchido'}}<br> <!-- ?? -> Valor default-->   
         Telefone: ({{$fornecedor['ddd'] ?? ''}}) {{$fornecedor['telefone'] ?? ''}}<br> <!--(00) 0000-0000 -->
         <hr>
-    @endforeach
+    @empty
+        Não existem fornecedores cadastrados    
+    @endforelse
 @endisset
