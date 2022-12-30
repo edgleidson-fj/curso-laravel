71ffb8b986f500c37d07dcecee91c5f57ef2c08a
Edgleidson <edgleidson.jesus>
2022-12-30 07:58:11 -0300

    Blade - @while

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -20,12 +20,14 @@
 
 <!--isset retorna True se a variavel estiver definida-->
 @isset($fornecedores)
-    <!--for -->
-    @for($i=0; isset($fornecedores[$i]); $i++)
+    @php $i = 0 @endphp
+    <!--while -->
+    @while(isset($fornecedores[$i]))
         Fornecedor: {{$fornecedores[$i]['nome']}}<br>
         Status: {{$fornecedores[$i]['status']}}<br>
         CNPJ: {{$fornecedores[$i]['cnpj'] ?? 'Dado não foi preenchido'}}<br> <!-- ?? -> Valor default-->   
         Telefone: ({{$fornecedores[$i]['ddd'] ?? ''}}) {{$fornecedores[$i]['telefone'] ?? ''}}<br> <!--(00) 0000-0000 -->
         <hr>
-    @endfor      
+        @php $i++ @endphp 
+    @endwhile
 @endisset
