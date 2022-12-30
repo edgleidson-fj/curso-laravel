baa2a036ae2bea3025a48a1ad1448b697d3cf26b
Edgleidson <edgleidson.jesus>
2022-12-30 08:40:53 -0300

    Blade - Escapando a tag de impressão do Blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -22,8 +22,8 @@
 @isset($fornecedores)
     <!--forelse - Obs: Se por acaso o Array estiver vazio, o fluxo será desviado para o bloco empty -->
     @forelse($fornecedores as $indice => $fornecedor)
-        Fornecedor: {{$fornecedor['nome']}}<br>
-        Status: {{$fornecedor['status']}}<br>
+        Fornecedor: @{{$fornecedor['nome']}}<br>  <!--@{}} - Tag de impressão do Blade -->
+        Status: @{{$fornecedor['status']}}<br>
         CNPJ: {{$fornecedor['cnpj'] ?? 'Dado não foi preenchido'}}<br> <!-- ?? -> Valor default-->   
         Telefone: ({{$fornecedor['ddd'] ?? ''}}) {{$fornecedor['telefone'] ?? ''}}<br> <!--(00) 0000-0000 -->
         <hr>
