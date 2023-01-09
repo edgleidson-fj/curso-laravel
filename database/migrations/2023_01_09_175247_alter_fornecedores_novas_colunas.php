b07576d203ef1ba12d4ba24d2ed5b58b0759f71b
Edgleidson <edgleidson.jesus>
2023-01-09 15:00:38 -0300

    Migration - Adicionando campos a uma tabela

---
+++ database/migrations/2023_01_09_175247_alter_fornecedores_novas_colunas.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterFornecedoresNovasColunas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Adiciona as colunas UF e Email na tabela Fornecedores
+        Schema::table('fornecedores', function (Blueprint $table){
+            $table->string('uf', 2);
+            $table->string('email', 150);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
