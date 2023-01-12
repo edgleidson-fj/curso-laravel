6d66cae6b1f8d7e650671abbb50fe48bbd98828c
Edgleidson <edgleidson.jesus@saude.ba.gov.br>
2023-01-12 09:14:07 -0300

    Eloquent - Inserindo registros com Create e Fillable

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -8,4 +8,6 @@ class Fornecedor extends Model
 {
     //Definir outro nome para tabela. Obs: Por padrão ficaria 'fornecedors'
     protected $table = 'fornecedores';
+
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
