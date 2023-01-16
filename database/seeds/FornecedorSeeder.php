b774a6b4421d570c917a81d19a14d9832b09a25a
Edgleidson <edgleidson.jesus@saude.ba.gov.br>
2023-01-16 15:46:18 -0300

    Seeders - Parte 1

---
+++ database/seeds/FornecedorSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Fornecedor;
+
+class FornecedorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Obs: Formas de execução de Insert
+
+        //1ª forma
+        $f = new Fornecedor();
+        $f->nome = 'Fornecedor 123';
+        $f->site = 'fornecedor123.com.br';
+        $f->uf = 'DF';
+        $f->email = 'fornecedor123@teste.com';
+        $f->save();  
+
+        //2ª forma
+        Fornecedor::create([
+            'nome'=>'Fornecedor 200',
+            'site'=> 'fornecedor200.com.br',
+            'uf'=>'RS',
+            'email'=>'contato@teste.com'
+        ]);
+
+        //3ª forma
+        DB::table('fornecedores')->insert([
+            'nome'=>'Fornecedor 300',
+            'site'=> 'fornecedor300.com.br',
+            'uf'=>'DF',
+            'email'=>'contato@teste.com'
+        ]);
+    }
+}
