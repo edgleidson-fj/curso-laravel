829f5b8762840f7c683c7657be7fecf8a067263a
Edgleidson <edgleidson.jesus@saude.ba.gov.br>
2023-01-19 16:09:54 -0300

    Entendendo o objeto Request

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -6,8 +6,15 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato(){
+    public function contato(Request $request){
         //var_dump($_POST);
+        //dd($request->all()); 
+
+        echo'<pre>';
+        print_r($request->all()); //Request com todos dados
+        echo'</br>';
+        echo $request->input('nome'); //Request de dados especificos
+
         return view('site.contato', ['titulo'=>'Contato']);
     }
 }
