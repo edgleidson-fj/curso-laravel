33f10c6c41fe67ab82e2a1ff95bc25a3d28b18c6
Edgleidson Ferreira de Jesus <edgleidson.jesus@saude.ba.gov.br>
2023-02-07 17:24:26 -0300

    Ajustando o formulário de contato na ota principal

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -27,7 +27,7 @@
             <div class="contato">
                 <h1>Contato</h1>
                 <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
-                @component('site.layout._components.form_contato', ['classe'=>'borda-branca'])  <!--Component -- Obs: Para adicionar View em forma de componente-->                    
+                @component('site.layout._components.form_contato', ['classe'=>'borda-branca', 'motivo_contatos'=>$motivo_contatos])  <!--Component -- Obs: Para adicionar View em forma de componente-->                    
                 @endcomponent
             </div>
         </div>
