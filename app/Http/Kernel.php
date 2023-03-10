759a839605239500b00d54701b76ca134bbf718f
Edgleidson Ferreira de Jesus <edgleidson.jesus@saude.ba.gov.br>
2023-03-10 10:23:53 -0300

    Apelidando Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             //-------------------------------------------------------
-            \App\Http\Middleware\LogAcessoMiddleware::class, //Log de acesso
+            //\App\Http\Middleware\LogAcessoMiddleware::class, //Log de acesso
 
         ],
 
@@ -66,5 +66,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        //---------------------------------------------------------------------
+        'log.acesso'=>  \App\Http\Middleware\LogAcessoMiddleware::class, //Log de acesso
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/', 'PrincipalController@principal')->name('site.index')->middleware('log.acesso'); //Acessando Middleware através do apelido definido no Kernel
 Route::get('/sobrenos', 'SobrenosController@sobrenos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
